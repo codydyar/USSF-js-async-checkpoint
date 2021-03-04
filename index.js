a70e0365efd5c467a8e89720532d7098f21df4c1
Cody Dyar <codydyar@gmail.com>
2021-03-04 19:03:02 +0100

    final edits before grading

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('this is my file!')
+fetch('https://pokeapi.co/api/v2/type/charmander') {
+    const response = await fetch('https://pokeapi.co/api/v2/type/charmander');
+    const data = await response.json();
+
+    return data;
+}
+console.log(data)
+
+
+
+
